523c65b24e365effb575ce85cf807ac113524865
BigMax1982 <maksym.chornyy@gmail.com>
2020-04-02 21:24:26 +0200

    Add powershell scripts for test

---
+++ Script/GetCustomers.ps1
@@ -0,0 +1,7 @@
+$url = 'https://localhost:32768/'
+
+Invoke-RestMethod -Uri $url'customers'
+
+Invoke-RestMethod -Uri $url'customers/d4fece72-4333-4227-93d3-a8b1e98f1bd2'
+
+Read-Host -Prompt "Press Enter to exit"
