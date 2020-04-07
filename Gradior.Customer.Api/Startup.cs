a9d781ecf778bad6138c70364b582d0ffb37a188
BigMax1982 <maksym.chornyy@gmail.com>
2020-04-07 21:38:57 +0200

    Allow origin

--- Gradior.Customer.Api/Startup.cs
+++ Gradior.Customer.Api/Startup.cs
@@ -21,11 +21,25 @@ namespace Gradior.Customer.Api
       Configuration = configuration;
     }
 
+    readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
     public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddCors(options =>
+      {
+        options.AddPolicy(MyAllowSpecificOrigins,
+        builder =>
+        {
+          builder.WithOrigins("http://example.com",
+                                  "http://www.contoso.com")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowAnyOrigin();
+        });
+      });
+
       services.AddControllers();
 
       services.AddSwaggerGen(c =>
@@ -62,6 +76,8 @@ namespace Gradior.Customer.Api
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseCors(MyAllowSpecificOrigins);
 
       app.UseAuthorization();
 
